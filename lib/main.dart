49fc584ea1945ce4eb7db1e3e209ed1c4d201091
dtengeri <dtengeri@gmail.com>
2023-10-13 11:20:26 +0200

    feat: basic login layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,62 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Hello World!'),
+        ),
+        body: Column(
+          children: [
+            const FlutterLogo(
+              size: 128,
+            ),
+            const Text('Hello World!'),
+            const EmailTextField(),
+            const PasswordTextField(),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Login'),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class PasswordTextField extends StatelessWidget {
+  const PasswordTextField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Icon(Icons.lock),
+        Expanded(child: TextField()),
+      ],
+    );
+  }
+}
+
+class EmailTextField extends StatelessWidget {
+  const EmailTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Icon(Icons.email),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              label: Text('Email'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
