289a4c7273a2b6784646e122c8e58e6277cde759
dtengeri <dtengeri@gmail.com>
2023-10-13 12:29:39 +0200

    feat: initial pizza list

---
+++ lib/features/pizza_order/presentation/pizza_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PizzaListScreen extends StatelessWidget {
+  const PizzaListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
