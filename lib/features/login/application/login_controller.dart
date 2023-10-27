a7c2afe62ddfdb8bae8d238ed0148b74ceeed8be
dtengeri <dtengeri@gmail.com>
2023-10-27 14:32:33 +0200

    feat: implement login

--- lib/features/login/application/login_controller.dart
+++ lib/features/login/application/login_controller.dart
@@ -1,1 +1,39 @@
-class LoginController {}
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pizza_order_app/features/auth/application/auth_controller.dart';
+import 'package:pizza_order_app/features/core/domain/user.dart';
+import 'package:pizza_order_app/features/login/domain/login_form.dart';
+import 'package:pizza_order_app/features/login/infrastructure/providers.dart';
+
+class LoginController extends Notifier<LoginForm> {
+  @override
+  LoginForm build() {
+    return const LoginForm(
+      email: '',
+      password: '',
+    );
+  }
+
+  void updateEmail(String email) {
+    state = state.copyWith(email: email);
+  }
+
+  void updatePassword(String password) {
+    state = state.copyWith(password: password);
+  }
+
+  Future<User> login() async {
+    if (!state.isValid) {
+      throw Exception('form not valid');
+    }
+    final user = await ref.read(loginRepositoryProvider).login(
+          email: state.email,
+          password: state.password,
+        );
+    ref.read(authControllerProvider.notifier).signIn(user);
+    return user;
+  }
+}
+
+final loginControllerProvider = NotifierProvider<LoginController, LoginForm>(
+  () => LoginController(),
+);
--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pizza_order_app/features/core/presentation/email_text_field.dart';
 import 'package:pizza_order_app/features/core/presentation/password_text_field.dart';
+import 'package:pizza_order_app/features/login/application/login_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loginForm = ref.watch(loginControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pizza Order'),
@@ -19,10 +22,18 @@ class LoginScreen extends StatelessWidget {
             size: 128,
           ),
           const Text('Welcome!'),
-          const EmailTextField(),
-          const PasswordTextField(),
+          EmailTextField(
+            onChanged: ref.read(loginControllerProvider.notifier).updateEmail,
+            errorText: loginForm.emailErrorText,
+          ),
+          PasswordTextField(
+            onChanged:
+                ref.read(loginControllerProvider.notifier).updatePassword,
+            errorText: loginForm.passwordErrorText,
+          ),
           ElevatedButton(
             onPressed: () {
+              ref.read(loginControllerProvider.notifier).login();
               context.go('/pizzas');
             },
             child: const Text('Login'),
